6ad05164f0fc4e5fa137e0f06285d156a072f11e
Gherman Sorana <ghermansorana@yahoo.com>
2022-10-25 08:42:29 +0300

    Add files via upload

---
+++ Semester1/ASC/hw2_lab/HW2.asm
@@ -0,0 +1,31 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    a db 14
+    b db 20
+    c db 23
+    d db 10
+    
+;(b+c)+(a+b-d)
+; our code starts here
+segment code use32 class=code
+    start:
+            mov AL, [b]
+            add AL, [c]   ; b + c
+            mov BL, [a]
+            add BL, [b]     ; a + b
+            sub BL, [d]      ; a+ b - d
+            add AL, BL      ;(b+c)+(a+b-d)
+            
+            
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Semester1/ASC/hw2_lab/hw31.asm
@@ -0,0 +1,37 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    a db 10
+    b db 20
+    c db 5
+    d dw 70
+
+;[d-(a+b)*2]/c
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov AL, [a]
+        add AL, [b]  ; a + b
+        mov BL, 2
+        mul BL       ; (a+b)/2
+        mov BX, [d]
+        sub BX, AX    ; d - (a+b)/2
+        mov AX, BX
+        mov DL, [c]  
+        div DL        ; [d-(a+b)*2]/c
+        
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program

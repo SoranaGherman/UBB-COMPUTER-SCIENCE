97bbcc71c3fcf664eee807b6205121ce8a1d21d6
Gherman Sorana <ghermansorana@yahoo.com>
2023-06-27 10:07:57 +0300

    Add files via upload

---
+++ Semester4/web/lab9/tomcatConf/src/main/java/com/assignment9/model/User.java
@@ -0,0 +1,24 @@
+package com.assignment9.model;
+
+public class User {
+    private final String username;
+    private final String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+}
